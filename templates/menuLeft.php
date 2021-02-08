63ecddde6dd9e52f8944c9dc9933b0673f3acf4f
Jan Napieralski <janek2000-2000@wp.pl>
2021-02-08 15:43:26 +0100

    menuLeft done

--- templates/menuLeft.php
+++ templates/menuLeft.php
@@ -13,36 +13,49 @@
     </section>
 
     <section id="user-section">
-        <div id="profile-picture">
-            <img src="../static/images/john.png"/>
-        </div>
+        <div id="profile-picture" style="background-image: url('../static/images/john.jpg')"></div>
+
+        <h1 id="user-name-text">John Doe</h1>
+
+        <a href="#">
+            <div id="myPanel">
+                My panel
+            </div>
+        </a>
+
+        <a href="#">
+            <div id="logOut">
+                Log out
+            </div>
+        </a>
+       
     </section>
 
 
     <ul id="option-list">
 
-        <a href="#" style="text-decoration: none;">
+        <a href="#">
             <li class="menu-option">
                 <span class="material-icons option-icons"> home</span>
                 <span class="option-text">Home</span>
             </li>
         </a>
         
-        <a href="#" style="text-decoration: none;">
+        <a href="#">
             <li class="menu-option">
                 <span class="material-icons option-icons">chat</span>
                 <span class="option-text">Chat</span>
             </li>
         </a>
 
-        <a href="#" style="text-decoration: none;">
+        <a href="#">
             <li class="menu-option">
                 <span class="material-icons option-icons">settings_suggest</span>
                 <span class="option-text">Settings</span>
             </li>
         </a>
 
-        <a href="#" style="text-decoration: none;">
+        <a href="#">
             <li class="menu-option">
                 <span class="material-icons option-icons">alternate_email</span>
                 <span class="option-text">About</span>
