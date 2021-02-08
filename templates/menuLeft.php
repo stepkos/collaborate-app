608b9503dd19c4ff84467e8bdca6a9d531c4a080
Jan Napieralski <janek2000-2000@wp.pl>
2021-02-08 15:10:39 +0100

    testing menuLeft

---
+++ templates/menuLeft.php
@@ -0,0 +1,54 @@
+<style>
+    <?php
+        require_once("../static/css/menuLeft.css");
+    ?>
+</style>
+
+
+
+
+<aside id="menuLeft">
+    <section id="collaborate-logo">
+        Collaborate
+    </section>
+
+    <section id="user-section">
+        <div id="profile-picture">
+            <img src="../static/images/john.png"/>
+        </div>
+    </section>
+
+
+    <ul id="option-list">
+
+        <a href="#" style="text-decoration: none;">
+            <li class="menu-option">
+                <span class="material-icons option-icons"> home</span>
+                <span class="option-text">Home</span>
+            </li>
+        </a>
+        
+        <a href="#" style="text-decoration: none;">
+            <li class="menu-option">
+                <span class="material-icons option-icons">chat</span>
+                <span class="option-text">Chat</span>
+            </li>
+        </a>
+
+        <a href="#" style="text-decoration: none;">
+            <li class="menu-option">
+                <span class="material-icons option-icons">settings_suggest</span>
+                <span class="option-text">Settings</span>
+            </li>
+        </a>
+
+        <a href="#" style="text-decoration: none;">
+            <li class="menu-option">
+                <span class="material-icons option-icons">alternate_email</span>
+                <span class="option-text">About</span>
+            </li>
+        </a>
+
+
+    </ul>
+</aside>
---
+++ templates/metadata.php
@@ -0,0 +1,14 @@
+
+
+<!--
+    linki do wspólnego css i czcionek Google
+-->
+
+<meta charset="UTF-8">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="../static/css/common.css" type="text/css">
