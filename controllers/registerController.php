883d3592cde4bd9064ac0b8cb05ed1ca3c5acddb
stepkos <stepkos@wp.pl>
2021-02-09 20:08:18 +0100

    correct register

--- controllers/registerController.php
+++ controllers/registerController.php
@@ -3,18 +3,27 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     require_once "models/UserModel.php";
-    
-    User::register(
-        $_POST['email'],
-        $_POST['name'],
-        $_POST['surname'],
-        $_POST['password'],
-        $_POST['confirm_password']
-    );
+    require_once "validators/formValidators.php";
 
-    header('Location: login');
-    exit();
+    if (FormValidators::register(
+            $_POST['email'],
+            $_POST['name'],
+            $_POST['surname'],
+            $_POST['password'],
+            $_POST['confirm_password']
+        ))
+    {
+        User::register(
+            $_POST['email'],
+            $_POST['name'],
+            $_POST['surname'],
+            $_POST['password']
+        );
     
+        header('Location: login');
+        exit();
+    }
+
 }
 
 require_once "views/register.php";
