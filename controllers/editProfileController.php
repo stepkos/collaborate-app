266f45c7553054341fd14eb728e98b0373b708b1
stepkos <stepkos@wp.pl>
2021-02-12 00:46:47 +0100

    edit profile validator

--- controllers/editProfileController.php
+++ controllers/editProfileController.php
@@ -4,22 +4,17 @@ session_start();
 require_once "tools/loginRequired.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    require_once "models/editProfileModel.php";
+    
     require_once "validators/editProfileValidator.php";
-
+    
     if (editProfileValidator()) {
-
+        
         // User::login();
-
+        
         header('Location: home');
         exit();
     }
 }
 
-
-require_once "models/editProfileModel.php";
-$technologies = EditProfileModel::getTechnologies();
-
-
+require_once "models/editProfileModelGET.php";
 require_once "views/editProfileView.php";
--- validators/editProfileValidator.php
+++ validators/editProfileValidator.php
@@ -2,18 +2,52 @@
 
 function editProfileValidator() {
 
-    if (isset($_FILES['profile_picture']))
-        $_SESSION['form_profile_picture'] = $_FILES['profile_picture'];
+    // if (isset($_FILES['profile_picture']))
+    //     $_SESSION['form_profile_picture'] = $_FILES['profile_picture'];
 
+    $_SESSION['form_description'] = '';
     if (isset($_POST['description']))
         $_SESSION['form_description'] = $_POST['description'];
 
+    $_SESSION['form_technologies'] = '';
+    if (isset($_POST['technologies']))
+        foreach($_POST['technologies'] as $tech)
+            $_SESSION['form_technologies'] .= $tech.',';
 
-    echo $_SESSION['form_description'];
-    exit();
+    // NO VALUE!
+    if (isset($_POST['email']))
+        $_SESSION['form_email'] = $_POST['email'];
+  
+    // NO VALUE!
+    if (isset($_POST['password']))
+        $_SESSION['form_password'] = $_POST['password'];
 
-    
+    //-------------------------------//
+    //            LINKS              //
+    //-------------------------------//
+    $_SESSION['form_link_portfolio'] = '';
+    if (isset($_POST['link-portfolio']))
+        $_SESSION['form_link_portfolio'] = $_POST['link-portfolio'];
 
+    $_SESSION['form_link_github'] = '';
+    if (isset($_POST['link-github']))
+        $_SESSION['form_link_github'] = $_POST['link-github'];
+
+    $_SESSION['form_link_facebook'] = '';
+    if (isset($_POST['link-facebook']))
+        $_SESSION['form_link_facebook'] = $_POST['link-facebook'];
+
+    $_SESSION['form_link_linkedln'] = '';
+    if (isset($_POST['link-linkedln']))
+        $_SESSION['form_link_linkedln'] = $_POST['link-linkedln'];
+
+    $_SESSION['form_link_instagram'] = '';
+    if (isset($_POST['link-instagram']))
+        $_SESSION['form_link_instagram'] = $_POST['link-instagram'];
+
+    $_SESSION['form_link_twitter'] = '';
+    if (isset($_POST['link-twitter']))
+        $_SESSION['form_link_twitter'] = $_POST['link-twitter'];
 
     return true;
 }
