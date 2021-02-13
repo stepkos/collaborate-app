d1d457a6a30e6dc0c7cc9043fd9d72efe1b64259
stepkos <stepkos@wp.pl>
2021-02-13 23:44:51 +0100

    let's start post editProfile data

---
+++ models/editProfileModelPOST.php
@@ -0,0 +1,5 @@
+<?php
+
+// call insert_further_user_data(1, "jej jestem janek", NULL, NULL, "Github,Linkedln","https://github.com/,https//linkedln.com", "React,HTML,CSS");
+
+$db = require_once "db/connect.php";
