929d9401dedf2d0c183ae4b1efbf7b9826e55e81
Kamil Paczkowski <pivotguy888@gmail.com>
2021-02-16 14:53:34 +0100

    Update addProjectView.php

--- views/addProjectView.php
+++ views/addProjectView.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once "templates/metadata.php"; ?>
+    <link rel="stylesheet" href="static/css/addProject.css" type="text/css"/>
     <title>Document</title>
 </head>
 <body>
-    Legends says there is gonna be a proper view here.
+    <?php require_once "templates/menuLeft.php"; ?>
+
+    <main>
+        <div id="form_holder">
+            <div id="project_image" style="background-image: url('static/images/obraz.png')">
+                <div id="project_type">Web</div>
+            </div>
+
+        </div>
+    </main>
 </body>
 </html>
