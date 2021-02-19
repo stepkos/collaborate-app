83505ba2de110d999888980a18944e4696a62fc7
stepkos <stepkos@wp.pl>
2021-02-19 14:34:39 +0100

    clean form validators

---
+++ tools/cleanFormErrors.php
@@ -0,0 +1,6 @@
+<?php
+unset($_SESSION['form_error_email']);
+unset($_SESSION['form_error_name']);
+unset($_SESSION['form_error_surname']);
+unset($_SESSION['form_error_password']);
+unset($_SESSION['form_error_confirm_password']);
