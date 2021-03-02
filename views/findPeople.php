38b7800f4e9bb66de3aa6e70ab02fb17113db97d
stepkos <stepkos@wp.pl>
2021-03-02 22:32:25 +0100

    findPeople - person card :D

--- views/findPeople.php
+++ views/findPeople.php
@@ -56,46 +56,26 @@
                     </span>
                 </div>
 
-                           
-
-                <!-- <div class='project-card-find' style='z-index:2' id='2'>
-                    <a href='{$link}'>
-                        <div class='project-img-holder' style='background-image:url(static/images/obraz.png)'></div>
-                        <div class='project-category'>WEB</div>
-
-                        <div class='project-bottom-holder-find'>
-                            <h1 class='project-title'>
-                                Nazwa
-                            </h1>
-
-                            <div class='project-tech-holder'>
-                                <div class='project-tech' style='background-color: red;'>javascript</div>
-                                <div class='project-tech' style='background-color: green;'>php</div>
-                            </div>
-                        </div>
-                    </a>
-                </div> -->
 
                 <div class='project-card-find' style='z-index:2' id='2'>
-                    <a href='{$link}'>
-                        <div class='project-img-holder' style='background-image:url(static/images/john.jpg)'>
-                        
-                        
-                    </div>
-                    <div class="project-details-icon">i</div>
-                    <!-- <div class='project-category'>WEB</div> -->
-
-
-                        <div class='project-bottom-holder-find'>
+                    <a href='LINK DO PROFILU'>
+                        <div class='person-img' style='background-image:url(static/images/john.jpg)'></div>
+            
+                        <div class='person-info'>
                             <h1 class='project-title'>
                                 Jan Napieralski
                             </h1>
 
+                            <div class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur ipsa voluptatum at dolorum dignissimos necessitatibus, sequi, voluptas placeat iusto quis, nulla error beatae aperiam similique? Iure esse consectetur impedit quia.
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, obcaecati repudiandae! Inventore dolor neque totam quas ipsum amet placeat labore aspernatur sed omnis at eum, porro iure ex magni cum!</div>
+
                             <div class='project-tech-holder'>
                                 <div class='project-tech' style='background-color: red;'>javascript</div>
                                 <div class='project-tech' style='background-color: green;'>php</div>
+                                <div class='project-tech' style='background-color: purple;'>C#</div>
                             </div>
                         </div>
+
                     </a>
                 </div>
 
