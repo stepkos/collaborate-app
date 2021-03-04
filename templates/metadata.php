aa4358eae5ed57529e3c7f7d717d2543f66e30f7
stepkos <stepkos@wp.pl>
2021-03-04 13:22:02 +0100

    import other weight of font

--- templates/metadata.php
+++ templates/metadata.php
@@ -10,7 +10,7 @@
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
 <link rel="stylesheet" href=<?php echo ROOT_URL."static/css/common.css";?> type="text/css"/>
